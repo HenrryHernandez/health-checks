740b137b545155120c8ca7a5b149010ad020c64d
SlickGeek <jenrruhndz@gmail.com>
2020-08-03 16:01:44 -0600

    Allow printing more than one error message

--- all_checks.py
+++ all_checks.py
@@ -4,9 +4,11 @@ import sys
 import os
 import shutil
 
+
 def check_reboot():
     """Returns True if the computer has a pending reboot."""
     return os.path.exists("/run/reboot-requierd")
+
 
 def check_disk_full(disk, min_gb, min_percent):
     """Returns true if there isn't enough disk space, False otherwise"""
@@ -24,19 +26,22 @@ def check_root_full():
     """Returns True if the root partition is full, False otherwise"""
     return check_disk_full(disk="/", min_gb=2, min_percent=10)
 
+
 def main():
     
     checks=[
         (check_reboot, "Pending Reboot"),
         (check_root_full, "Root partition full"),
     ]
+    everything_ok = True
 
     for check, msg in checks:
         if check():
             print(msg)
-            sys.exit(1)
- 
-    print("in")
+            everything_ok = False
+    if not everything_ok:
+        sys.exit(1) 
+
     if check_reboot():
         print("Pending Reboot.")
         sys.exit(1)
