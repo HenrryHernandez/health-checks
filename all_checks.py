ba7bdd631bb120b6871a83ac11b1618177d00699
SlickGeek <jenrruhndz@gmail.com>
2020-08-03 15:06:47 -0600

    recently created the file again

--- all_checks.py
+++ all_checks.py
@@ -1,1 +1,31 @@
+#!/usr/bin/env python3
+
 import sys
+import os
+import shutil
+
+def check_reboot():
+    """Returns True if the computer has a pending reboot."""
+    return os.path.exists("/run/reboot-requierd")
+
+def check_disk_full(disk, min_gb, min_percent):
+    """Returns true if there isn't enough disk space, False otherwise"""
+    du = shutil.disk_usage(disk)
+    # Calculate the percentage of free space
+    percent_free = 100 * du.free / du.total
+    # Calculate how many free gigabytes
+    gigabytes_free = du.free / 2**30
+    if gigabytes_free < min_gb or percent_free < min_percent:
+        return True
+    return False
+
+def main():
+    if check_reboot():
+        print("Pending Reboot.")
+        sys.exit(1)
+    if check_disk_full(disk="/", min_gb = 2, min_percent = 10):
+        print("Disk full.")
+        sys.exit(1)
+
+    print("Everything ok")
+    sys.exit(0)
